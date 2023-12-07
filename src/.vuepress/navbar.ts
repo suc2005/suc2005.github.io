fa035187598051eeb6e82f36a02755c063f7b781
suc2005 <suc536184@gmail.com>
2023-12-07 23:02:01 +0800

    5th commit

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -10,7 +10,8 @@ export default navbar([
       {text: "小组学习", icon:"graduation-cap",link:"小组学习"},
       {text: "游戏推荐",icon: "gamepad",link: "游戏推荐",},
       {text: "学校新闻",icon: "newspaper",link:"学校新闻"},
-      {text: "随笔",icon: "pen-nib",link:"随笔"}
+      {text: "随笔",icon: "pen-nib",link:"随笔"},
+      {text: "代码笔记",icon: "code",link:"代码笔记"}
     ]
   },
   {
